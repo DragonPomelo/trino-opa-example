d996d7447c322fcde6d4a1d17cb141501c624d92
nadavgross <nadavgross7@gmail.com>
2024-01-16 10:58:47 +0200

    added lru_cache to mongo handler

--- abac_api/src/mongo_handler.py
+++ abac_api/src/mongo_handler.py
@@ -1,34 +1,36 @@
 import pymongo
+import functools
 
 
+@functools.lru_cache(maxsize=128)
 def initialize_mongo_client():
     client = pymongo.MongoClient("mongodb://attribute_db:27017/")
     db = client["attributes_db"]
     return db
 
 
-# this function receives a collection name and fetches all the documents from it
+@functools.lru_cache(maxsize=128)
 def get_collection(collection_name: str):
     db = initialize_mongo_client()
     collection = db[collection_name]
     return collection.find({})
 
 
-# this function receives a collection name, a key, and returns all the documents that have this key
+@functools.lru_cache(maxsize=128)
 def get_collection_by_key(collection_name: str, key: str):
     db = initialize_mongo_client()
     collection = db[collection_name]
     return collection.find({key: {"$exists": True}})
 
 
-# this function receives a collection name, a key, and a value, and returns all the documents that have this key and value
+@functools.lru_cache(maxsize=128)
 def get_collection_by_key_value(collection_name: str, key: str, value):
     db = initialize_mongo_client()
     collection = db[collection_name]
     return collection.find({key: value})
 
 
-# this function receives a collection name, list of keys and list of values, and returns all the documents that have these keys and values
+@functools.lru_cache(maxsize=128)
 def get_collection_by_keys_values(collection_name: str, keys: list, values: list):
     db = initialize_mongo_client()
     collection = db[collection_name]
